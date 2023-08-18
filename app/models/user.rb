4bcc7d8755ac3758c052ea582af9ca17a72fe8dc
Rafael Guimaraes <raf.plguimaraes@gmail.com>
2023-08-18 11:48:43 -0300

    Feat: Configurando Rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
 
   belongs_to :bank_agency
   has_one :checking_account
