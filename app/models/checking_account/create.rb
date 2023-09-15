0d306b1f141114d2bfd41389d7e6f3cfec7c30e8
Celso <cportella99@gmail.com>
2023-09-15 14:37:02 -0300

    feat: teste para o checking_account

--- app/models/checking_account/create.rb
+++ app/models/checking_account/create.rb
@@ -1,5 +1,9 @@
 module CheckingAccount
   class Create < ::Micro::Case
     attribute :user_id, :ammount
+
+    def call
+      byebug
+    end
   end
 end
--- spec/models/checking_account_spec.rb
+++ spec/models/checking_account_spec.rb
@@ -2,16 +2,39 @@
 
 require 'rails_helper'
 
-RSpec.describe CheckingAccount::Record, type: :model do
+RSpec.describe CheckingAccount::Create, type: :model do
   describe '.call' do
     describe 'failures' do
       context 'Quando o usuário for vazio' do
         let(:user_id) { nil }
+        let(:amount) { 0 }
 
         it 'Retorna uma failure' do
-          result = describe_class.call
+          result = described_class.call
 
           expect(result).to be_a_failure
+        end
+      end
+      context 'Quando o usuário for invalido' do
+        let(:user_id) { '****' }
+        let(:amount) { 0 }
+
+        it 'Retorna uma failure' do
+          result = described_class.call
+
+          expect(result).to be_a_failure
+        end
+      end
+    end
+    describe 'success' do
+      context 'Quando a criação for sucesso' do
+        let(:user_id) { '1' }
+        let(:amount) { 0 }
+
+        it 'Retorna um sucesso' do
+          result = described_class.call
+
+          expect(result).to be_a_success
         end
       end
     end
